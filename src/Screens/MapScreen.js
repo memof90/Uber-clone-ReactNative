d73a3c3a49efe5cdfd936f39e437e96722065815
memo figueredo <memof80@gmail.com>
2021-08-21 16:54:53 -0500

    created new component to navigate Card

--- src/Screens/MapScreen.js
+++ src/Screens/MapScreen.js
@@ -1,21 +1,34 @@
 //import liraries
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+// NAVIGATION
+import { createStackNavigator } from '@react-navigation/stack';
 // Tailiwind
 import tw from 'tailwind-react-native-classnames';
 // Components
 import Map from '../Components/Map/Map';
+import NavigateCard from '../Components/cardNavigate/NavigateCard';
 
 
 // create a component
 const MapScreen = () => {
+
+    const Stack = createStackNavigator();
+
     return (
         <View>
-            <View style={tw `h-2/3`}>
+            <View style={tw `h-1/2`}>
                 <Map />
             </View>
-            <View style={tw `h-1`}></View>
+            <View style={tw `h-1/2`}>
+                <Stack.Navigator>
+                <Stack.Screen name="NavigateCard" 
+                component={NavigateCard} 
+                options={{headerShown: false}}
+                />
+
+                </Stack.Navigator>
+            </View>
         </View>
     );
 };
