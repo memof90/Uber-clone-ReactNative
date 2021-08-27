6d007d091808442e1061ff08824faf41e6f87ab1
memo figueredo <memof80@gmail.com>
2021-08-27 13:05:30 -0500

    added comment to map and geberate effect to zoom in to map markers

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -28,7 +28,7 @@ const Map = () => {
 // setUp refresh Map to only draw map when the user introduce origin into map and not re render component efificently to render component
     useEffect(() => {
         if (!origin || !destination) return;
-        // Zoom & fit to markers
+        // Zoom & fit to markers map
         mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
             edgePadding:  { top: 50, right: 50, bottom: 50, left: 50}
         })
