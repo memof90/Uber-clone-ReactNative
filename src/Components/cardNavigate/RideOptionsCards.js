dc8f7a6d58c2cdc353ace0a9c83de54b27aa02c8
memo figueredo <memof80@gmail.com>
2021-08-27 16:53:01 -0500

    created flatlist to section select Ride into rideOptions Components

--- src/Components/cardNavigate/RideOptionsCards.js
+++ src/Components/cardNavigate/RideOptionsCards.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, SafeAreaView,TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView,TouchableOpacity, FlatList, Image } from 'react-native';
 
 // REACT NATIVE ELEMENTS ICON
 import { Icon } from 'react-native-elements';
@@ -13,6 +13,28 @@ import tw from 'tailwind-react-native-classnames';
 const RideOptionsCards = () => {
      // SetUp Navegation
      const navigation = useNavigation();
+
+    //  setUp Rides
+    const data = [
+        {
+            id: "uber-X-123",
+            title: "UberX",
+            multipier: 1,
+            image: "https://links.papareact.com/3pn"
+        },
+        {
+            id: "uber-XL-456",
+            title: "Uber XL",
+            multipier: 1.2,
+            image: "https://links.papareact.com/5w8"
+        },
+        {
+            id: "uber-LUX-789",
+            title: "Uber LUX",
+            multipier: 1.75,
+            image: "https://links.papareact.com/7pf"
+        },
+    ];
     return (
         <SafeAreaView style={tw `bg-white flex-grow`}>
         <View>
@@ -23,7 +45,22 @@ const RideOptionsCards = () => {
                 <Icon type="fontawesome" name="chevron-left" />
             </TouchableOpacity>
             <Text style={tw `text-center py-5 text-xl`}>Select a Ride</Text>
-
+        <FlatList 
+            data={data}
+            keyExtractor={item => item.id}
+            renderItem={({item : {id,title,multipier,image}, item}) => (
+                <TouchableOpacity>
+                    <Image 
+                        style={{
+                            width: 100,
+                            height: 100,
+                            resizeMode: "contain"
+                        }}
+                        source={{ uri: image }}
+                    />
+                </TouchableOpacity>
+            )}
+        />
         </View>
         </SafeAreaView>
     )
