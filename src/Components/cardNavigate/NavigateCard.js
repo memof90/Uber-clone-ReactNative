c1193f71eadef0b3d689d460263f9fdfff5f8e8a
memo figueredo <memof80@gmail.com>
2021-08-27 15:20:10 -0500

    added navegation to buttons

--- src/Components/cardNavigate/NavigateCard.js
+++ src/Components/cardNavigate/NavigateCard.js
@@ -61,13 +61,16 @@ const NavigateCard = () => {
                 </View>
                 <NavFavorites />
                 <View style={tw `flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
-                    <TouchableOpacity style={tw `flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}>
+                    <TouchableOpacity 
+                    style={tw `flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+                    onPress={()=> navigation.navigate('RideOptionsCards')}
+                    >
                         <Icon name="car" type="font-awesome" color="#fff" size={16}/>
                         <Text style={tw `text-white text-center`}>Rides</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={tw `flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
                         <Icon name="fast-food-outline" type="ionicon" color="#000" size={16}/>
-                        <Text style={tw `text-center`}>Rides</Text>
+                        <Text style={tw `text-center`}>Eats</Text>
                     </TouchableOpacity>
                 </View>
             </View>
