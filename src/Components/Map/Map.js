8d78895f47346b4fc2872fd8d448acc4febe3856
memo figueredo <memof80@gmail.com>
2021-08-27 13:02:58 -0500

    added to destination padding to map

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -29,7 +29,9 @@ const Map = () => {
     useEffect(() => {
         if (!origin || !destination) return;
         // Zoom & fit to markers
-        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'])
+        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+            edgePadding:  { top: 50, right: 50, bottom: 50, left: 50}
+        })
     }, [origin, destination])
 
     return (
