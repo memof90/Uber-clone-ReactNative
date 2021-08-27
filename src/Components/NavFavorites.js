266d1a5b195f02c47128222d89c0a09118103b2f
memo figueredo <memof80@gmail.com>
2021-08-27 14:23:56 -0500

    added new  navFavorites routes

--- src/Components/NavFavorites.js
+++ src/Components/NavFavorites.js
@@ -1,11 +1,40 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
+
+// REACT NATIVE ELEMENTS ICON
+import { Icon } from 'react-native-elements';
+
+// TAILWIND
+import tw from 'tailwind-react-native-classnames';
+
+// REACT NAVIGATION
+import { useNavigation } from '@react-navigation/native';
 
 const NavFavorites = () => {
+
+    const data = [
+        {
+            id: "123",
+            icon: "home",
+            location: "Home",
+            destination: "Code Street, London, Uk"
+        },
+        {
+            id: "456",
+            icon: "briefcase",
+            location: "Work",
+            destination: "London Eye, London, Uk"
+        },
+    ];
+
     return (
-        <View>
-            <Text>Nav Fvorites</Text>
-        </View>
+        <FlatList 
+            data={data}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+                <TouchableOpacity />
+            )}
+        />
     )
 }
 
