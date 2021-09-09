8e12bb20f286aecc5beedc28270d8105b8ab00a7
memo figueredo <memof80@gmail.com>
2021-09-09 18:09:31 -0500

    adedd menu to navigate to homeScreen

--- src/Screens/MapScreen.js
+++ src/Screens/MapScreen.js
@@ -1,10 +1,13 @@
 //import liraries
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 // NAVIGATION
 import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
 // Tailiwind
 import tw from 'tailwind-react-native-classnames';
+// REACT NATIVE ELEMENTS ICON
+import { Icon } from 'react-native-elements';
 // Components
 import Map from '../Components/Map/Map';
 import NavigateCard from '../Components/cardNavigate/NavigateCard';
@@ -13,11 +16,21 @@ import RideOptionsCards from '../Components/cardNavigate/RideOptionsCards';
 
 // create a component
 const MapScreen = () => {
-
+    // setUp StackNavegation
     const Stack = createStackNavigator();
+    // setup navegation
+    const navegation = useNavigation();
 
     return (
         <View>
+        {/* // added menu to back button  */}
+        <TouchableOpacity 
+        onPress={() => navegation.navigate("HomeScreen")}
+        style={tw `bg-gray-200 absolute top-16 left-8 z-50 
+        p-3 rounded-full shadow-lg
+        `}>
+            <Icon name="menu" />
+        </TouchableOpacity>
             <View style={tw `h-1/2`}>
                 <Map />
             </View>
